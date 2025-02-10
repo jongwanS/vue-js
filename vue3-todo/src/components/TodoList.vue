1c3776262f2e7f2c6b73051f68e6c25020708119
jongwanS <lim8848@gmail.com>
2025-02-10 17:22:42 +0900

    commit

--- vue3-todo/src/components/TodoList.vue
+++ vue3-todo/src/components/TodoList.vue
@@ -1,11 +1,27 @@
 <template>
     <div>
-        Todo List
+        <ul>
+            <li v-for="(item, index) in todoItems" :key="index">
+                {{ item }}
+            </li>
+        </ul>
     </div>
 </template>
 
 <script setup>
+import { ref, onBeforeMount } from 'vue'
+const todoItems = ref([])
 
+onBeforeMount(() => {
+    console.log('mounted in the composition api!')
+    if (localStorage.length > 0) {
+        for (var i = 0; i < localStorage.length; i++) {
+            const storageKey = localStorage.key(i)
+            todoItems.value.push(localStorage.getItem(storageKey));
+        }
+    }
+    console.log(todoItems.value)
+})
 </script>
 
 <style scoped></style>
